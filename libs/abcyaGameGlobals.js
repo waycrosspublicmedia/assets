4c8fe34f5480f62f2b2c7747f24771e1017c7f19
mll-enh <attackgames080@gmail.com>
2025-04-24 21:12:20 -0400

    Update abcyaGameGlobals.js

--- libs/abcyaGameGlobals.js
+++ libs/abcyaGameGlobals.js
@@ -3,7 +3,7 @@
 
         function init() {
             var host = window.location.hostname;
-            var domain = host === 'www.ixl.com' || host === 'ixl.com' ? 'ixl.com' : 'abcya.com';
+            var domain = host === 'www.diesmos.com' || host === 'diesmos.com' ? 'ixl.com' : 'diesmos.com';
             document.domain = domain;
             abcyaWindow = window.parent;
             abcyaWindow.abcya = abcyaWindow.abcya || {};
